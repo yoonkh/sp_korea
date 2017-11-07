72a9ce6d7a72e38fbc5162b663109d93082e105b
Aaron So <jae.woo@blackrubystudio.com>
2017-11-07 16:04:28 +0900

    설문조사 기능 연동 (merge commit into #master from #survey)
    Flask-base와 기존에 만들어 두었던 survey 연동

--- app/main/views.py
+++ app/main/views.py
@@ -1,27 +1,19 @@
-from flask import render_template, request, app, url_for
-# from ..models import EditableHTML
-from werkzeug.utils import redirect
+from flask import render_template
+from ..models import EditableHTML
 
-from pattern import find_pattern
 from . import main
 
 
 @main.route('/')
-def basic():
-    return redirect(url_for('.survey_index'))
+def index():
+    return render_template('main/index.html')
 
 
-@main.route('/survey')
-def survey_index():
-    return render_template('survey/index.html')
-
-
-@main.route('/survey/<page>')
-def survey_page(page):
-    res_code = request.args.get('resCode', None)
-    if res_code:
-        return render_template('/survey/result.html', result=find_pattern(res_code))
-    return render_template('/survey/'+page+'.html')
+@main.route('/about')
+def about():
+    editable_html_obj = EditableHTML.get_editable_html('about')
+    return render_template('main/about.html',
+                           editable_html_obj=editable_html_obj)
 
 
 
