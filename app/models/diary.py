d6492245117cbdd62c6508795e4f8bb8857ab293
yoonkh <zizou0812@gmail.com>
2017-11-09 16:29:26 +0900

    add class Health(Diary)

--- app/models/diary.py
+++ app/models/diary.py
@@ -14,6 +14,8 @@ diary_users = db.Table('diary_users',
                        db.Column('diary_id', db.Integer, db.ForeignKey('diary.id')),
                        db.Column('food_id', db.Integer, db.ForeignKey('food.id')),
                        db.Column('water_id', db.Integer, db.ForeignKey('water.id')),
+                       db.Column('sleep_id', db.Integer, db.ForeignKey('sleep.id')),
+                       db.Column('health_id', db.Integer, db.ForeignKey('health.id')),
                        )
 
 
@@ -27,16 +29,21 @@ class Diary(db.Model):
     # title = db.Column(db.String(255))
 
     # 운동일지 다이어리
+    # 운동 1,2,3,4
     exe_per_day = db.Column(db.String(10))
-
+    # 운동종류 입력
     exe_per_day_type = db.Column(db.String(10))
-
+    # 횟수 입력
     rep = db.Column(db.String(10))
 
     # 운동사진 업로드필드
     # exe_photo = db.Column(db.)
+
+    # 운동이름 입력
     exe_name = db.Column(db.String(10))
+    # 소모 칼로리 입력
     exe_kcal = db.Column(db.String(10))
+    # 운동 시간 입력
     exe_time = db.Column(db.String(10))
 
 
@@ -59,22 +66,30 @@ class Diary(db.Model):
 class Food(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+
+    # 식단 (음식) 종류
     breakfast_food_type = db.Column(db.String(10))
     lunch_food_type = db.Column(db.String(10))
     dinner_food_type = db.Column(db.String(10))
     snack_food_type = db.Column(db.String(10))
     etc_food_type = db.Column(db.String(10))
+
+    # 식단 칼로리 입력
     kcal_food = db.Column(db.String(10))
 
     # 식단사진 업로드필드
     # photo_food = db.Column(db.)
 
+    # 음식 이름 입력
     food_name = db.Column(db.String(10))
+
+    # 음식 칼로리 입력
     food_kcal = db.Column(db.String(10))
 
     # 식사선택 select field
     # food_select = db.
 
+    # 음식 메모 입력란
     food_memo = db.Column(db.String(255))
 
 
@@ -89,11 +104,11 @@ class Food(db.Model):
 class Water(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-
+    # 물 종류
     water_type = db.Column(db.String(10))
-
+    # 물 이름 입력
     water_type_name = db.Column(db.String(10))
-
+    # 물 용량 입력
     water_ml = db.Column(db.String(10))
 
     create_dttm = db.Column(db.DateTime, nullable=False)
@@ -101,8 +116,38 @@ class Water(db.Model):
     users = db.relationship("User", secondary="diary_users", backref=db.backref('waters'))
 
 
+class Sleep(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # 수면시간 입력필드입니다
+    sleep_time = db.Column(db.String(10))
+
+    create_dttm = db.Column(db.DateTime, nullable=False)
+
+    users = db.relationship("User", secondary="diary_users", backref=db.backref('sleeps'))
 
 
+class Health(db.Model):
 
+    id = db.Column(db.Integer, primary_key=True)
+
+    # 혈압 - select field
+    # blood_pressure = db.
+    # 혈당 - select field
+    # blood_sugar = db.
+    # 심리 - select field
+    # mind = db.
+
+    # 혈압 입력
+    blood_pressure_mmhg = db.Column(db.String(10))
+    # 혈당 입력
+    blood_suger_mddl = db.Column(db.String(10))
+    # 심리 입력
+    mind_mg_dl = db.Column(db.String(10))
+
+    create_dttm = db.Column(db.DateTime, nullable=False)
+
+    users = db.relationship("User", secondary="diary_users", backref=db.backref('healths'))
 
 
